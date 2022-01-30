4c657c4f313b6ca51f0563eaa2b7db3cd77e52b7
sagupta153 <sagupta153@yahoo.com>
2022-01-30 11:42:49 +0530

    added 2 new model code functions

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,11 @@
 print ("adding all ml related code here")
+
+def model1:
+    print ("code for model - 1")
+
+def model2:
+    print ("code for model - 2")
+
+def model3:
+    print ("code for model - 3")
+
